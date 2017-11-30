074f6e129da5cef51ace8a633ed1e347a65be91a
Trikita73 <diachenkoandrii7@gmail.com>
2017-11-30 19:15:01 +0200

    section_4

--- js/common.js
+++ js/common.js
@@ -9,6 +9,19 @@ $(document).ready(function() {
 	$(".section-content .info-item").equalHeights();
 	$(".s1_bottom .info-item").equalHeights();
 
+	$(".section_4").waypoint(function() {
+
+		$(".section_4 .card").each(function(index) {
+			var ths = $(this);
+			setInterval(function() {
+				ths.removeClass("card-off").addClass("card-on");
+			}, 200*index);
+		});
+
+	}, {
+		offset : "20%"
+	});
+
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
 		ga("send", "event", "goal", "goal");
