627bd36bd8077f53acc12bd606f83f9330f3fd90
Trikita73 <diachenkoandrii7@gmail.com>
2017-12-01 16:01:27 +0200

    section_5

--- js/common.js
+++ js/common.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
 		$(".main-mnu").slideToggle();
 	});
 
+	//EqualHeights для выравнивания блоков
 	$(".section-content .info-item").equalHeights();
 	$(".s1_bottom .info-item").equalHeights();
 
+	//Waypoint для последовательной анимации 
 	$(".section_4").waypoint(function() {
 
 		$(".section_4 .card").each(function(index) {
@@ -21,6 +23,25 @@ $(document).ready(function() {
 	}, {
 		offset : "20%"
 	});
+
+
+	$(".section_5").waypoint(function() {
+
+		$(".section_5 .tc-item").each(function(index) {
+			var ths = $(this);
+			setTimeout(function() {
+				//svg animation прорисовывание
+				var myAnimation = new DrawFillSVG({
+					elementId: "tc-svg-" + index 
+				});
+				ths.removeClass("").addClass("");
+			}, 700*index);
+		});
+
+	}, {
+		offset : "20%"
+	});
+
 
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
