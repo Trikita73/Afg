a46d6307d8d725c711a079d0ecc595a8eface4dd
andrii <diachenkoandrii7@gmail.com>
2018-01-05 13:44:36 +0200

    mail.php

--- mail.php
+++ mail.php
@@ -6,13 +6,13 @@ $sitename  = "Armata Financical Group";
 $subject   = "Новая заявка с сайта \"$sitename\"";
 
 $name = trim($_POST["name"]);
-$phome = trim($_POST["email"]);
+$phome = trim($_POST["phone"]);
 $formname = trim($_POST["formname"]);
 
 $message = "
 Форма: $formname <br>
-E-mail: $email <br>
-Имя: $phone
+Имя: $name <br>
+Телефон: $phone
 ";
 
 mail($recepient, $subject, $message, "From: $frm_name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
